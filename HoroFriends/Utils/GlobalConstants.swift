1d741248223c00dac04d5e54f1a1d49e9fc24074
alangogoro <cayne9160@gmail.com>
2021-01-04 17:48:50 +0800

    lil commit

---
+++ HoroFriends/Utils/GlobalConstants.swift
@@ -0,0 +1,21 @@
+//
+//  GlobalConstants.swift
+//  HoroFriends
+//
+//  Created by usr on 2020/12/30.
+//
+
+let SESSION_TOKEN = "iGYGa4nkNetgOiFb2zIDDarOzEAi50zn"
+let FB_ID = ""
+let APPLE_ID = ""
+
+/**
+ **粗體** *斜體* 一般文字
+ ```
+ 程式碼區塊
+ ```
+ - Remark: ⭐️ 重要提示
+ - Warning: ⚠️ 警告
+ - Requires: ➡️ 需要條件說明
+ */
+public func markdown() {}
